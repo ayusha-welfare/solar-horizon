324b5b10fd0aac2703a41300f0bb7dd337440f93
vinodsharma13 <vinodsharma.india@gmail.com>
2025-12-07 14:09:50 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: Replace 'solar-horizon' with your repository name if different
-  // Set to '/' if you are deploying to username.github.io directly
-  base: '/solar-horizon/',
 })
